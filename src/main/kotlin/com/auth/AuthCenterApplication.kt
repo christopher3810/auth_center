2054a2d1e84a0c434696953daa03a5759ef8a3ec
sungminyang <lonesome661@naver.com>
2025-03-17 02:29:35 +0900

    Feat : 컨피규레이션 추가

--- src/main/kotlin/com/auth/AuthCenterApplication.kt
+++ src/main/kotlin/com/auth/AuthCenterApplication.kt
@@ -1,9 +1,12 @@
 package com.auth
 
+import com.auth.infrastructure.config.JwtConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtConfig::class)
 class AuthCenterApplication
 
 fun main(args: Array<String>) {
