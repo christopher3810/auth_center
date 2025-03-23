838dd1035ae9c01415173ecfb818748ecab473bd
sungminyang <lonesome661@naver.com>
2025-03-23 20:59:02 +0900

    Feat : test container 용 custom annotation 추가

---
+++ src/test/kotlin/com/auth/annotation/IntegrationTest.kt
@@ -0,0 +1,22 @@
+package com.auth.annotation
+
+import com.auth.config.TestDatabaseConfig
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.core.annotation.AliasFor
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.transaction.annotation.Transactional
+import org.testcontainers.junit.jupiter.Testcontainers
+import kotlin.annotation.AnnotationRetention.RUNTIME
+import kotlin.annotation.AnnotationTarget.CLASS
+import kotlin.reflect.KClass
+
+@Target(CLASS)
+@Retention(RUNTIME)
+@SpringBootTest
+@ActiveProfiles("test")
+@Testcontainers
+@Transactional
+annotation class IntegrationTest (
+    @get:AliasFor(annotation = SpringBootTest::class, attribute = "classes")
+    val classes: Array<KClass<*>> = [TestDatabaseConfig::class]
+)
---
+++ src/test/kotlin/com/auth/annotation/JpaIntegrationTest.kt
@@ -0,0 +1,23 @@
+package com.auth.annotation
+
+import com.auth.config.TestDatabaseConfig
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.context.annotation.Import
+import org.springframework.core.annotation.AliasFor
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.transaction.annotation.Transactional
+import org.testcontainers.junit.jupiter.Testcontainers
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@DataJpaTest
+@ActiveProfiles("test")
+@Testcontainers
+@Transactional
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(TestDatabaseConfig::class)
+annotation class JpaIntegrationTest(
+    @get:AliasFor(annotation = DataJpaTest::class, attribute = "properties")
+    val properties: Array<String> = []
+)
